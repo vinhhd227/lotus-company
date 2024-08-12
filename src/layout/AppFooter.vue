0f2133eb669188b930851c497f67c27367f9862c
vinhhd <vinhhd227@gmail.com>
2024-08-12 22:37:10 +0700

    init

---
+++ src/layout/AppFooter.vue
@@ -0,0 +1,134 @@
+<template>
+  <footer class="footer">
+    <div class="container pt-2" id="contact">
+      <div class="row row-grid mt-1">
+        <div class="col-lg-4">
+          <h5 class="mt-3">Liên hệ</h5>
+          <ul class="pl-0">
+            <li class="list-group-item border-0 bg-trans p-1">
+              Công ty cổ phần vật tư thiết bị Lotus
+            </li>
+            <li class="list-group-item border-0 bg-trans p-1">
+              <i class="ni ni-mobile-button mr-2"></i>
+              <a href="tel:0903455202"> 0903455202 </a>
+            </li>
+            <li class="list-group-item border-0 bg-trans p-1">
+              <i class="ni ni-planet mr-2"></i>
+              <a href="#"> lotuscompany.com.vn </a>
+            </li>
+            <li class="list-group-item border-0 bg-trans p-1">
+              <i class="ni ni-email-83 mr-2"></i>
+              <a href="mailto:contact@lotusco.com.vn">
+                contact@lotusco.com.vn
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-4 mt-0">
+          <h5 class="mt-3">Địa chỉ</h5>
+          <p class="mt-3">
+            <i class="ni ni-square-pin mr-1"></i>
+            Phòng 601, tầng 6, SKY CITY TOWER A, 88 Láng Hạ, Quận Đống Đa, Hà
+            Nội
+          </p>
+        </div>
+        <div class="col-lg-4 mt-0">
+          <h5 class="mt-3">Theo dõi hoạt động của chúng tôi</h5>
+          <p class="mt-3">
+            Phương châm xây dựng một đội ngũ cán bộ, nhân viên có tính chuyên
+            nghiệp cao, coi đó là tiền đề để tạo nên sức mạnh trong kinh doanh
+            và thúc đẩy công ty phát triển một cách vững chắc.
+          </p>
+          <div class="mt-3">
+            <base-button
+              tag="a"
+              href="#"
+              type="warning"
+              icon="fa fa-twitter"
+              rounded
+              icon-only
+            ></base-button>
+            <base-button
+              tag="a"
+              href="#"
+              type="warning"
+              icon="fa fa-facebook"
+              rounded
+              icon-only
+            ></base-button>
+            <base-button
+              tag="a"
+              href="#"
+              type="warning"
+              icon="fa fa-linkedin"
+              rounded
+              icon-only
+            ></base-button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <hr />
+      <div class="row align-items-center justify-content-md-between">
+        <div class="col-md-6">
+          <div class="copyright">
+            &copy; {{ year }}
+            <a
+              href="https://www.creative-tim.com"
+              target="_blank"
+              rel="noopener"
+              >Lotus company</a
+            >
+          </div>
+        </div>
+        <div class="col-md-6">
+          <ul class="nav nav-footer justify-content-end">
+            <li class="nav-item">
+              <a
+                href="https://www.creative-tim.com"
+                class="nav-link"
+                target="_blank"
+                rel="noopener"
+                >Trang chủ</a
+              >
+            </li>
+            <li class="nav-item">
+              <a
+                href="https://www.creative-tim.com/presentation"
+                class="nav-link"
+                target="_blank"
+                rel="noopener"
+                >Giới thiệu</a
+              >
+            </li>
+            <li class="nav-item">
+              <a
+                href="http://blog.creative-tim.com"
+                class="nav-link"
+                target="_blank"
+                rel="noopener"
+                >Liên hệ</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  name: "app-footer",
+  data() {
+    return {
+      year: new Date().getFullYear(),
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.bg-trans {
+  background-color: transparent !important;
+}
+</style>
---
+++ src/layout/AppHeader.vue
@@ -0,0 +1,92 @@
+<template>
+  <header class="header-global">
+    <base-nav class="navbar-main" transparent type="" effect="light" expand>
+      <router-link slot="brand" class="navbar-brand mr-lg-5" to="/">
+        <!-- <img src="img/brand/white.png" alt="logo" /> -->
+         <p class="m-0">LOTUS COMPANY</p>
+      </router-link>
+
+      <div class="row" slot="content-header" slot-scope="{ closeMenu }">
+        <div class="col-6 collapse-brand">
+          <a
+            href="https://demos.creative-tim.com/vue-argon-design-system/documentation/"
+          >
+            <img src="img/brand/blue.png" />
+          </a>
+        </div>
+        <div class="col-6 collapse-close">
+          <close-button @click="closeMenu"></close-button>
+        </div>
+      </div>
+
+      <ul class="navbar-nav navbar-nav-hover align-items-lg-center">
+        <li>
+          <a
+            slot="title"
+            href="#"
+            class="nav-link"
+            data-toggle="dropdown"
+            role="button"
+          >
+            <!-- <i class="ni ni-ui-04 d-lg-none"></i> -->
+            <span class="nav-link-inner--text">Trang chủ</span>
+          </a>
+        </li>
+        <li>
+          <a
+            slot="title"
+            href="#"
+            class="nav-link"
+            data-toggle="dropdown"
+            role="button"
+          >
+            <!-- <i class="ni ni-ui-04 d-lg-none"></i> -->
+            <span class="nav-link-inner--text">Giới thiệu</span>
+          </a>
+        </li>
+        <base-dropdown tag="li" class="nav-item">
+          <a
+            slot="title"
+            href="#"
+            class="nav-link"
+            data-toggle="dropdown"
+            role="button"
+          >
+            <i class="ni ni-collection d-lg-none"></i>
+            <span class="nav-link-inner--text">Giải pháp</span>
+          </a>
+          <a class="dropdown-item">Thiết bị</a>
+          <a class="dropdown-item">Hóa chất công nghiệp</a>
+          <a class="dropdown-item">Tư vấn chuyển giao công nghệ</a>
+          <a class="dropdown-item">Vận chuyển, kho bãi</a>
+        </base-dropdown>
+        <li>
+          <a
+            slot="title"
+            href="#contact"
+            class="nav-link"
+            data-toggle="dropdown"
+            role="button"
+          >
+            <!-- <i class="ni ni-ui-04 d-lg-none"></i> -->
+            <span class="nav-link-inner--text">Liên hệ</span>
+          </a>
+        </li>
+      </ul>
+    </base-nav>
+  </header>
+</template>
+<script>
+import BaseNav from "@/components/BaseNav";
+import BaseDropdown from "@/components/BaseDropdown";
+import CloseButton from "@/components/CloseButton";
+
+export default {
+  components: {
+    BaseNav,
+    CloseButton,
+    BaseDropdown,
+  },
+};
+</script>
+<style></style>
